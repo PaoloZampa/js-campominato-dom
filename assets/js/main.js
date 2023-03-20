9b244a82400fd2106192f5ac913f371cff9258e7
PaoloZampa <nlmp.paolozampa@gmail.com>
2023-03-20 15:44:10 +0100

    initial commit trying to not bestemmiare

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+/* Creare una griglia 8x8
+
+ Ogni volta che clicco su un quadrato si colora di verde */
+
+// Creo una variabile dove salvo il container delle 64 celle (container-grid)
+
+const container = document.querySelector(".container");
+
+// creo un max numero di celle
+
+let nMax = 64;
+
+// creo un ciclo n volte per le celle (dove n in questo caso sta per 64)
+
+function createSquare() {
+for (let i = 0; i < nMax; i++) {
+  const cell = `<div class="cell"></div>`;
+  container.innerHTML += cell;
+  cell.innerHTML += nMax[i]
+}
+}
+
+// seleziono una cella che ha classe cell e active
+
+const cellEl = document.querySelectorAll(".cell")
+console.log(cellEl)
+
+// aggiungo eventlisner per la classe active ciclando per la lunghezza dell'array cellEl
+
+function colorSquare() {
+  for (let i = 0; i < cellEl.length; i++) {
+    const thisCell = cellEl[i];
+    console.log(thisCell)
+    thisCell.addEventListener("click", function () {
+      thisCell.classList.toggle("bg_green")
+      console.log("Changed the color")
+    })
+  }
+}
+
