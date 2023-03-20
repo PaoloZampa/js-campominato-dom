289454840dbc3596463002e615950833e68d4f56
PaoloZampa <nlmp.paolozampa@gmail.com>
2023-03-20 16:13:48 +0100

    add color on click

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,26 +13,22 @@ let nMax = 100;
 // creo un ciclo n volte per le celle (dove n in questo caso sta per 64)
 
 function createSquare() {
-for (let i = 0; i < nMax; i++) {
-  const cell = `<div class="cell">${i + 1}</div>`;
-  container.innerHTML += cell;
-}
-}
+  for (let i = 0; i < nMax; i++) {
+    const cell = `<div class="cell">${i + 1}</div>`;
+    container.innerHTML += cell;
+  }
 
-// seleziono una cella che ha classe cell e active
-const cellEl = document.querySelectorAll(".cell")
-
-// aggiungo eventlisner per la classe active ciclando per la lunghezza dell'array cellEl
-
-function colorSquare() {
-  for (let i = 0; i < cellEl.length; i++) {
-    const thisCell = cellEl[i];
+  const cellEl = document.querySelectorAll(".cell")
+  console.log(cellEl)
+  for (let j = 0; j < cellEl.length; j++) {
+    const thisCell = cellEl[j];
     console.log(thisCell)
     thisCell.addEventListener("click", function () {
-      thisCell.classList.toggle("bg_green")
+      thisCell.classList.toggle("active")
       console.log("Changed the color")
     })
   }
+
 }
 
 
